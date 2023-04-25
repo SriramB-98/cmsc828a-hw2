e5b5df3164d5f581a67074856a57931f98823fd5
Shweta Bhardwaj <shwetabhardwaj44@gmail.com>
2023-04-24 20:35:49 -0400

    Delete run_eval.sh

--- Ensamble/run_eval.sh
+++
@@ -1,8 +0,0 @@
-
-
-
-python run_ensemble_no_trainer.py --model_name_or_path microsoft/resnet-18 --output_dir ./output_run1 --with_tracking >> out_train_terminal
-
-cp -r ./output_run1 ./output_run2
-
-python run_ensemble_no_trainer.py --model_name_or_path microsoft/resnet-18 --output_dir ./output_run2 --eval_only True --with_tracking >> out_eval_terminal
